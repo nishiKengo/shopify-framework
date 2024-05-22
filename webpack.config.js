3959f4eb4ee3fa3f1ce1387ac6f427500a2a7198
nishiKengo <138430002+nishiKengo@users.noreply.github.com>
2024-05-22 17:50:07 +0900

    shopifyストアにReactの環境構築とHeaderコンポーネントの作成
    npm init -y
    npm install react react-dom @babel/preset-react @babel/preset-env webpack webpack-cli babel-loader
    上記の順でコマンドを打ってから新しくsrcフォルダを作成、中にコンポーネントフォルダを作りheader.jsxを入れる
    ルートディレクトリに webpack.config.js babel.config.json を作成
    npx webpackで更新

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'assets'),
+    publicPath: '/assets/',
+    hot: true, // ホットリロードを有効に
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  mode: 'development',
+};
